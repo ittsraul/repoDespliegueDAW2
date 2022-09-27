3f22e63370e56dc6a612ad7c91110181550939fe
V1CTOR-CLOUD <victorbg189@gmail.com>
2022-09-28 01:47:38 +0200

    commit VICTOR en rama raul 5/10

--- index.php
+++ index.php
@@ -7,5 +7,9 @@ $result = $pepe + $juan;
 
 echo $result;
 
+function testing($name){    //echo "pepe: " . $pe; //echo "pe
+    echo "pepe: " . $name;    //echo "pepe: " . $juan; //echo "pepe
+}
+
 }
 
